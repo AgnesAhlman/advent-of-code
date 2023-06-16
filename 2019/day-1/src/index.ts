7c8abdf97e11ddc302461131ee696bfc43dd0046
Agnes Ahlman <ahlman.agnes@gmail.com>
2023-06-16 17:20:18 +0200

    2019 day-1 pt.1

--- 2019/day-1/src/index.ts
+++ 2019/day-1/src/index.ts
@@ -1,1 +1,28 @@
-console.log('hello world')
+const fs = require('fs');
+const path = require('path');
+
+const fuelForSingleModule = (mass: number): number => {
+  return Math.floor(mass / 3) - 2;
+};
+
+const calculateTotalFuel = (masses: number[]) => {
+  const totalFuel = masses.reduce((acc: number, currentMass: number) => {
+    const fuel = fuelForSingleModule(currentMass);
+    return acc + fuel;
+  }, 0);
+
+  return totalFuel;
+};
+
+try {
+  // Read the input file
+  const filePath = path.join(__dirname, '../input.txt');
+  const data = fs.readFileSync(filePath, 'utf8');
+
+  // Split the data into an array of numbers
+  const masses: number[] = data.trim().split('\n').map(Number);
+  const totalFuel = calculateTotalFuel(masses);
+  console.log(totalFuel);
+} catch (e) {
+  console.error(e);
+}
