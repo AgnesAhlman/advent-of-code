d88f54bf4e428713d206080692ab5ffa4376c730
Agnes Ahlman <ahlman.agnes@gmail.com>
2023-06-18 20:26:07 +0200

    renamed and cleaned up

--- 2019/day-2/src/index.ts
+++ 2019/day-2/src/index.ts
@@ -2,30 +2,27 @@ import fs from 'fs';
 import path from 'path';
 
 const program = (numbers: number[]) => {
-  let arr = numbers;
+  for (let i = 0; i < numbers.length; i += 4) {
+    const optCode = numbers[i];
+    const firstIndex = numbers[i + 1];
+    const secondIndex = numbers[i + 2];
+    const outputPos = numbers[i + 3];
 
-  for (let i = 0; i < arr.length; i += 4) {
-    const optCode = arr[i];
-    const firstIndex = arr[i + 1];
-    const secondIndex = arr[i + 2];
-    const numberOfIndex3 = arr[i + 3];
-
-    const firstNumber = arr[firstIndex];
-    const secondNumber = arr[secondIndex];
+    const noun = numbers[firstIndex];
+    const verb = numbers[secondIndex];
 
     if (optCode === 1) {
-      const totalSum = firstNumber + secondNumber;
-      arr[numberOfIndex3] = totalSum;
+      numbers[outputPos] = noun + verb;
     }
     if (optCode === 2) {
-      const newNumber = firstNumber * secondNumber;
-      arr[numberOfIndex3] = newNumber;
+      numbers[outputPos] = noun * verb;
     }
     if (optCode === 99) {
       console.log('end program');
+      break;
     }
   }
-  return arr[0];
+  return numbers[0];
 };
 
 try {
@@ -34,7 +31,7 @@ try {
   const data = fs.readFileSync(filePath, 'utf8');
 
   // Split the data into an array of numbers
-  const numbers: number[] = data.trim().split(',').map(Number);
+  const memory: number[] = data.trim().split(',').map(Number);
 
   const targetOutput = 19690720;
   let noun = 0;
@@ -44,34 +41,26 @@ try {
   for (noun = 0; noun <= 99; noun++) {
     for (verb = 0; verb <= 99; verb++) {
       // Create a new copy of the program
-      const programCopy = [...numbers];
+      const memoryCopy = [...memory];
 
-      // Set the noun and verb values
-      programCopy[1] = noun;
-      programCopy[2] = verb;
+      memoryCopy[1] = noun;
+      memoryCopy[2] = verb;
 
-      // Run the program
-      const output = program(programCopy);
+      const output = program(memoryCopy);
 
       if (output === targetOutput) {
-        // Found the correct noun and verb
-        console.log('Noun:', noun);
-        console.log('Verb:', verb);
-
-        // Calculate the answer
         const answer = 100 * noun + verb;
         console.log('Answer:', answer);
 
-        // Exit the loops
         noun = 100;
         verb = 100;
       }
     }
   }
 
-  numbers[1] = 12;
-  numbers[2] = 2;
-  const finalOutput = program(numbers);
+  memory[1] = 12;
+  memory[2] = 2;
+  const finalOutput = program(memory);
   console.log('finalOutput', finalOutput);
 } catch (e) {
   console.error(e);
