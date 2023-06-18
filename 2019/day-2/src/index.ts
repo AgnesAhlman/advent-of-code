59dcf19f93925da5db69871457882a32227e96af
Agnes Ahlman <ahlman.agnes@gmail.com>
2023-06-18 16:07:19 +0200

    day-2 pt2

--- 2019/day-2/src/index.ts
+++ 2019/day-2/src/index.ts
@@ -35,6 +35,40 @@ try {
 
   // Split the data into an array of numbers
   const numbers: number[] = data.trim().split(',').map(Number);
+
+  const targetOutput = 19690720;
+  let noun = 0;
+  let verb = 0;
+
+  // Find the noun and verb that produce the target output
+  for (noun = 0; noun <= 99; noun++) {
+    for (verb = 0; verb <= 99; verb++) {
+      // Create a new copy of the program
+      const programCopy = [...numbers];
+
+      // Set the noun and verb values
+      programCopy[1] = noun;
+      programCopy[2] = verb;
+
+      // Run the program
+      const output = program(programCopy);
+
+      if (output === targetOutput) {
+        // Found the correct noun and verb
+        console.log('Noun:', noun);
+        console.log('Verb:', verb);
+
+        // Calculate the answer
+        const answer = 100 * noun + verb;
+        console.log('Answer:', answer);
+
+        // Exit the loops
+        noun = 100;
+        verb = 100;
+      }
+    }
+  }
+
   numbers[1] = 12;
   numbers[2] = 2;
   const finalOutput = program(numbers);
